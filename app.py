6d673969cbe90f2e77f4d00fcba91c220c040d59
Abdelnacer Jabri <73711840+faizandev12@users.noreply.github.com>
2025-04-09 16:12:38 -0400

    commit

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello_world():
     return 'Welcome to Wild Rydes on ECS Fargate! by Jabri'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
